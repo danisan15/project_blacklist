f161cc9c1cc97efa61b03f3fff9cb68c0c476f88
Cesar00z <cesarbruzual89@gmail.com>
2023-07-31 17:20:32 -0400

    feat: add plans section and images se agregó la sección de planes y las imágenes de cada plan.

---
+++ frontend/src/components/PlanCard.jsx
@@ -0,0 +1,20 @@
+const PlanCard = ({img, featured, title, price, descrip, support, buttonText }) => {
+
+  const planClassName = featured ? "featured-plan-card" : "plan-card";
+  const includesSupport = support ? "(Incluye soporte)" : "(No incluye soporte)";
+
+  return (
+    <div className={planClassName}>
+      <article>
+        <h2>{title}</h2>
+        <img src={img} />
+        <p>{descrip}</p>
+        <h4>{includesSupport}</h4>
+        <h3>{price}</h3>
+        <button className="button-price">{buttonText}</button>
+      </article>
+    </div>
+  )
+}
+
+export default PlanCard
