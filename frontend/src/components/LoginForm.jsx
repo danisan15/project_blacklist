c8e8e6186e208d286ce7f8aa0c69b06507b523b7
danisan15 <daniel.santaella1@gmail.com>
2023-08-14 22:13:26 -0400

    fix: fixed login component

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -8,10 +8,7 @@ import { createClient } from "@supabase/supabase-js";
 export default function LoginForm() {
   const navigate = useNavigate();
 
-  const client = createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_KEY
-  );
+  const client = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_KEY);
 
   const {
     register,
